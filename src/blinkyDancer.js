b46ecfbd1201d444efd58e60ee1f12e98c928410
Steven Tu <stu562@gmail.com>
2019-01-09 23:24:44 -0800

    implement psuedoclassical style inheritance

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,40 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
+var BlinkyDancer = function(top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps); // inheriteance of Dancer prop
+  // top, left, timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-
-  var oldStep = blinkyDancer.step;
-
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
-
-  return blinkyDancer;
+  // var oldStep = blinkyDancer.step;
+  // this.oldStep = this.step;
+  // return blinkyDancer;
 };
+
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
+
+BlinkyDancer.prototype.step = function() {
+  Dancer.prototype.step.call(this);// this line is creating BlinkyDancer Step to equal Dancer Step function
+
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  this.$node.toggle();
+};
+
+
+//notes:
+//why is this apply to the window at the last test ?!?!?
+// var boundStep = Dancer.prototype.step.bind(this);
+// boundStep();
+// var new = new BlinkyDancer();
+// new.step()
+// BlinkyDancer.prototype.step = Dancer.prototype.step
+
+
+// toggle() is a jQuery method to show/hide the <span> tag.
+// See http://api.jquery.com/category/effects/ for this and
+// other effects you can use on a jQuery-wrapped html tag.
+// console.log('this node' + this.$node);
+
+//call 
+
+
+// BlinkyDancer.prototype.oldStep = Dancer.prototype.step;
